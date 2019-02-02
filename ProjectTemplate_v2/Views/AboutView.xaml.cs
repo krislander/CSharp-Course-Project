962e2089bb21d385de6396f8ca7f28694148b56e
krislander <zoro1503@abv.bg>
2019-02-02 18:05:57 +0200

    pics and stuff

--- ProjectTemplate_v2/Views/AboutView.xaml.cs
+++ ProjectTemplate_v2/Views/AboutView.xaml.cs
@@ -21,44 +21,88 @@ namespace ProjectTemplate_v2.Views
     /// </summary>
     public partial class AboutView : UserControl
     {
-        public ICommand CreatorsPage { get; private set; }
-        public ICommand DashboardPage { get; set; }
-        public ICommand SensorsPage { get; private set; }
-        public ICommand MapPage { get; private set; }
+        public string ImagePath1 { get; private set; }
+        public string ImagePath2 { get; private set; }
+        public string ImagePath3 { get; private set; }
+        public string ImagePath4 { get; private set; }
+        
 
         public AboutView()
         {
             InitializeComponent();
-            CreatorsPage = new DelegateCommand((object obj) => BuildCreatorsPage());
-            DashboardPage = new DelegateCommand((object obj) => BuildDashboardPage());
-            SensorsPage = new DelegateCommand((object obj) => BuildSensorsPage());
-            MapPage = new DelegateCommand((object obj) => BuildMapPage());
-
-            
         }
 
-        private void BuildMapPage()
+        private void CreatorsPage_Checked(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            //screenshot of creator1
+            card2.Visibility = Visibility.Visible;
+            ImagePath2 = @"/ProjectTemplate_v2;component/Resources/PicturesAboutPage/Dashboard.jpg";
+            txtBlock1Header.Text = "Lorem Ipsum";
+            txtBlock1Text.Text = "Lorem Ipsum";
+
+            //screenshot of creator2
+            card4.Visibility = Visibility.Visible;
+            ImagePath2 = @"/ProjectTemplate_v2;component/Resources/PicturesAboutPage/Dashboard.jpg";
+            txtBlock1Header.Text = "Lorem Ipsum";
+            txtBlock1Text.Text = "Lorem Ipsum";
         }
 
-        private void BuildSensorsPage()
+        private void DashboardPage_Checked(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            //screenshot of sensor from dashboard
+            card1.Visibility = Visibility.Visible;
+            ImagePath1 = @"\ProjectTemplate_v2\Resources\PicturesAboutPage\Dashboard.jpg";
+            txtBlock1Header.Text = "Lorem Ipsum";
+            txtBlock1Text.Text = "Lorem Ipsum";
+
+            //screenshot of burger menu
+            card2.Visibility = Visibility.Visible;
+            ImagePath2 = @"\ProjectTemplate_v2\Resources\PicturesAboutPage\Dashboard.jpg";
+            txtBlock1Header.Text = "Lorem Ipsum";
+            txtBlock1Text.Text = "Lorem Ipsum";
+
         }
 
-        private void BuildDashboardPage()
+        private void SensorsPage_Checked(object sender, RoutedEventArgs e)
         {
-            //ImageBox.Source;
-            TextBlockHeader.Text = "Lorem ipsum";
-            DownTextBlockText.Text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+            //screenshot of sensor list
+            card1.Visibility = Visibility.Visible;
+            ImagePath1 = @"\ProjectTemplate_v2\Resources\PicturesAboutPage\Dashboard.jpg";
+            txtBlock1Header.Text = "Lorem Ipsum";
+            txtBlock1Text.Text = "Lorem Ipsum";
 
+            //screenshot of edit form
+            card2.Visibility = Visibility.Visible;
+            ImagePath2 = @"\ProjectTemplate_v2\Resources\PicturesAboutPage\Dashboard.jpg";
+            txtBlock1Header.Text = "Lorem Ipsum";
+            txtBlock1Text.Text = "Lorem Ipsum";
+
+            //screenshot of popupBox in sensors
+            card2.Visibility = Visibility.Visible;
+            ImagePath2 = @"\ProjectTemplate_v2\Resources\PicturesAboutPage\Dashboard.jpg";
+            txtBlock1Header.Text = "Lorem Ipsum";
+            txtBlock1Text.Text = "Lorem Ipsum";
+
+            //screenshot of current value field
+            card2.Visibility = Visibility.Visible;
+            ImagePath2 = @"\ProjectTemplate_v2\Resources\PicturesAboutPage\Dashboard.jpg";
+            txtBlock1Header.Text = "Lorem Ipsum";
+            txtBlock1Text.Text = "Lorem Ipsum";
         }
 
-        private void BuildCreatorsPage()
+        private void MapPage_Checked(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            //screenshot of sensor from dashboard
+            card1.Visibility = Visibility.Visible;
+            ImagePath1 = @"\ProjectTemplate_v2\Resources\PicturesAboutPage\MapPage.jpg";
+            txtBlock1Header.Text = "Lorem Ipsum";
+            txtBlock1Text.Text = "Lorem Ipsum";
+
+            //screenshot of burger menu
+            card2.Visibility = Visibility.Visible;
+            ImagePath2 = @"\ProjectTemplate_v2\Resources\PicturesAboutPage\MapPage.jpg";
+            txtBlock1Header.Text = "Lorem Ipsum";
+            txtBlock1Text.Text = "Lorem Ipsum";
         }
-        
     }
 }
